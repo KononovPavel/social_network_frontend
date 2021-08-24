760cfd1708c8c3d5d88a12882492027e355e5aa4
KononovPavel <ermkonir@gmail.com>
2021-08-24 20:10:00 +0300

    layout login_page and add redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import p from '../Profile/Profile.module.css'
+import l from './login.module.css'
+
 const Login = () => {
     return (
         <div className={`${p.profile}`}>
-            sadas
+            <div className={l.loginBLock}>
+                <h1>Вход</h1>
+                <LoginForm/>
+            </div>
         </div>
     );
 };
+
+
+const LoginForm = () => {
+    return (
+        <React.Fragment>
+            <form>
+                <div>
+                    <input className={l.input} type="text" placeholder={'Логин'}/>
+                </div>
+                <div>
+                    <input className={l.input} type="password" placeholder={'Пароль'}/>
+                </div>
+                <div>
+                    <input type="checkbox"/>
+                    <span className={l.span}>Запомнить меня</span>
+                </div>
+                <div>
+                    <button className={l.button}>Войти</button>
+                </div>
+            </form>
+        </React.Fragment>
+    )
+}
+
 
 export default Login;
